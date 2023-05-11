ec2c6e78433ba8ca19d21936f40bbd5a893cc51e
Milad Remote <mniroee@gmail.com>
2023-05-11 19:14:13 +0430

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:myflutter/pages/home.dart';
 import 'package:myflutter/pages/skills.dart';
+import 'package:myflutter/pages/splash.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/',
-  routes: {
-      '/': (context) => const Home(),
-      '/skills' : (context) => Skills()
-  },
-));
-
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => SplashScreen(),
+        '/': (context) => const Home(),
+        '/skills': (context) => Skills()
+      },
+    ));
---
+++ lib/pages/splash.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 4),
+      () => Navigator.pushNamed(context, '/'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black54,
+      body: Center(
+        child: Text(
+          'This is a Test App',
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Colors.amber,
+          ),
+        ),
+      ),
+    );
+  }
+}
